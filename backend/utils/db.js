b4cb78e5625705d42d3b235739147e83913d809e
NITIN M. TELANG <117341980+nitintelang110@users.noreply.github.com>
2024-03-05 11:30:48 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 
 const db = mysql.createConnection({
     
-    host:"localhost",
+    host:"https://ems-tau-three.vercel.app/",
     user:"root",
     password:"",
     database:"ems"
